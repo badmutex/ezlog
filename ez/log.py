0824773b2129f41ff07cc9bd268269cc9e71709c
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2011-09-24 16:49:02 -0400

    simpler interface for logging from different modules, allowing them to have different levels

--- ez/log.py
+++ ez/log.py
@@ -1,33 +1,3 @@
-"""
-ABOUT
-  This module is intended to be used for logging purposes in other programs.
-  It provides a clean wrapping for the python-provided *logging* module.
-
-HOW TO USE:
-  import ez.log as logging
-  log = logging.setup()
-  log.set_level(logging.DEBUG)
-  ...
-  log.debug('oops')
-  log.info('hello')
-  log.warn('uh oh...')
-  log.error('mayday! mayday!')
-  log.critical('42 / 0')
-
-LEVELs:
-  In order:
-    DEBUG
-    INFO
-    WARNING
-    ERROR
-    CRITICAL
-"""
-
-
-################################################################################
-#                                                                              #
-################################################################################
-
 
 import logging as _logging
 
@@ -39,41 +9,31 @@ ERROR    = _logging.ERROR
 CRITICAL = _logging.CRITICAL
 
 
-def setup(format='%(asctime)s %(module)s %(levelname)s %(message)s', level=INFO):
+## defaults
+_DEFAULT_NAME  = 'root'
+_LOG_FORMAT    = '%(asctime)s %(module)s %(levelname)s %(message)s'
+_DEFAULT_LEVEL = INFO
+
+def setup(name=_DEFAULT_NAME, format=_LOG_FORMAT, level=_DEFAULT_LEVEL):
+    """
+    @param name (string)
+    @param format (string)
+    @param level (LEVEL)
+    @return (Logger)
+    """
+
     _logging.basicConfig(format=format, level=level)
-    return _logging.getLogger()
+    return _logging.getLogger(name)
 
 
-def set_level(lvl):
+def set_level(lvl, name=_DEFAULT_NAME):
     """
     Set the logging level.
     This should only be called once at the top level
 
     @param lvl (LEVEL): a logging level
-    @return (Logger)
+    @param name='root' (string)
     """
 
-    logger = _logging.getLogger()
+    logger = _logging.getLogger(name)
     logger.setLevel(lvl)
-
-
-def _message(lvl, msg, *args, **kws):
-    # name = kws.pop('name', 'root')
-    logger = _logging.getLogger()
-    logger.log(lvl, msg, *args, **kws)
-
-def debug(*args, **kws):
-    _message(DEBUG, *args, **kws)
-
-def info(*args, **kws):
-    _message(INFO, *args, **kws)
-
-def warn(*args, **kws):
-    _message(WARNING, *args, **kws)
-
-def error(*args, **kws):
-    _message(ERROR, *args, **kws)
-
-def critical(*args, **kws):
-    _message(CRITICAL, *args, **kws)
-

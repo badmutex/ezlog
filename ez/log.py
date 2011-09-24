0d4164f3cd36462e4b9b38eaca5f64f22c870145
Badi' Abdul-Wahid <abdulwahidc@gmail.com>
2011-09-23 23:58:28 -0400

    logging from different modules shows up in the output string

--- ez/log.py
+++ ez/log.py
@@ -4,9 +4,9 @@ ABOUT
   It provides a clean wrapping for the python-provided *logging* module.
 
 HOW TO USE:
-  import ez.log as log
-  log.setup(__name__)
-  log.set_level(log.DEBUG)
+  import ez.log as logging
+  log = logging.setup()
+  log.set_level(logging.DEBUG)
   ...
   log.debug('oops')
   log.info('hello')
@@ -29,72 +29,19 @@ LEVELs:
 ################################################################################
 
 
-import logging
-
-
-
-### module-level logger
-_DEFAULT_LOGGER = None
-
-def _get_logger():
-    """
-    Get the module-level logger
-    @raise ValueError, if the logger has not yet been set
-    """
-    global _DEFAULT_LOGGER
-    if _DEFAULT_LOGGER is None:
-        raise ValueError, 'Module-level logger has not yet been set'
-    return _DEFAULT_LOGGER
-
-def _set_logger(logger):
-    """set the module-level logger"""
-    global _DEFAULT_LOGGER
-    _DEFAULT_LOGGER = logger
-
+import logging as _logging
 
 ### logging levels
-DEBUG    = logging.DEBUG
-INFO     = logging.INFO
-WARNING  = logging.WARNING
-ERROR    = logging.ERROR
-CRITICAL = logging.CRITICAL
+DEBUG    = _logging.DEBUG
+INFO     = _logging.INFO
+WARNING  = _logging.WARNING
+ERROR    = _logging.ERROR
+CRITICAL = _logging.CRITICAL
 
 
-
-def make_logger(name, loggerLevel=INFO, handlerLevel=DEBUG, logformat='%(asctime)s %(name)s %(levelname)s %(message)s'):
-    """
-    @param name (string): a name for this logger
-    @param loggerLevel=INFO (LEVEL): the minimum logging level for the logger
-    @param handlerLevel=DEBUG (LEVEL): the minimum logging level the the handler
-    @param logformat='%(asctime)s %(name)s %(levelname)s %(message)s' (str)
-    @return (logging.Logger): a Logger from the python logger module for *name*
-    """
-
-    print 'name:', name
-
-    logger    = logging.getLogger(name)
-    logger.setLevel(loggerLevel)
-
-    handler   = logging.StreamHandler()
-    handler.setLevel(handlerLevel)
-
-    formatter = logging.Formatter(logformat)
-
-    handler.setFormatter(formatter)
-    logger.addHandler(handler)
-
-    return logger
-
-
-def setup(*args, **kws):
-    """
-    Sets up the module-level logger
-
-    @params *args, **kws: passed to *make_logger*
-    """
-    logger = make_logger(*args, **kws)
-    _set_logger(logger)
-
+def setup(format='%(asctime)s %(module)s %(levelname)s %(message)s', level=INFO):
+    _logging.basicConfig(format=format, level=level)
+    return _logging.getLogger()
 
 
 def set_level(lvl):
@@ -102,35 +49,31 @@ def set_level(lvl):
     Set the logging level.
     This should only be called once at the top level
 
-    @param logger (Logger)
     @param lvl (LEVEL): a logging level
-
-    Example:
-        log
-        log.setup_logger(__name__)
-        log.set_level(log.DEBUG)
+    @return (Logger)
     """
 
-    logger = _get_logger()
+    logger = _logging.getLogger()
     logger.setLevel(lvl)
 
 
 def _message(lvl, msg, *args, **kws):
-    logger = _get_logger()
+    # name = kws.pop('name', 'root')
+    logger = _logging.getLogger()
     logger.log(lvl, msg, *args, **kws)
 
 def debug(*args, **kws):
-    _message(logging.DEBUG, *args, **kws)
+    _message(DEBUG, *args, **kws)
 
 def info(*args, **kws):
-    _message(logging.INFO, *args, **kws)
+    _message(INFO, *args, **kws)
 
 def warn(*args, **kws):
-    _message(logging.WARNING, *args, **kws)
+    _message(WARNING, *args, **kws)
 
 def error(*args, **kws):
-    _message(logging.ERROR, *args, **kws)
+    _message(ERROR, *args, **kws)
 
 def critical(*args, **kws):
-    _message(logging.CRITICAL, *args, **kws)
+    _message(CRITICAL, *args, **kws)
 
